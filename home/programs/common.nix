c57732e11bc5f8c2b043a8f137ae33b0da76e25a
matt <matt@crate.dev>
2024-08-24 17:15:01 +0100

    rofi wayland

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -39,7 +39,7 @@
   # Manage incompatible .configs
   programs.spotify-player.enable = true; xdg.configFile."spotify-player".source = ./dots/spotify-player;
   programs.ncspot.enable = true; xdg.configFile."ncspot/credentials.json".source = ./dots/ncspot/credentials.json;
-#  programs.rofi.enable = true; xdg.configFile."rofi".source = ./dots/rofi;
+  xdg.configFile."rofi".source = ./dots/rofi;
 
   programs = {
     nix-index = {
