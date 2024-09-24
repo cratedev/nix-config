55adccfd50526ef00a99fe033bb07f4cd3a5e11f
cratedev <matt@crate.dev>
2024-09-24 18:33:07 +0100

    nas

---
+++ home/nas/default.nix
@@ -0,0 +1,20 @@
+{ ... }:
+{
+  imports = [
+#    ./automation.nix
+#    ./mediaserver.nix
+#    ./auth.nix
+#    ./cloudflare.nix
+#    ./crowdsec.nix
+#    ./financial.nix
+#    ./homepage.nix
+#    ./immich.nix
+#    ./jellystat.nix
+#    ./paperless.nix
+    ./search.nix
+#    ./tesla.nix
+#    ./tools.nix
+#    ./proxy.nix
+#    ./passwordmgr.nix
+  ];
+}
---
+++ home/nas/search.nix
@@ -0,0 +1,4 @@
+{ pkgs, config, ... }:
+{
+  programs.searchxng.enable = true;
+}
