4920bc2c063e0454d459177330b1ed3d8ffa96e1
cratedev <matt@crate.dev>
2024-10-14 13:48:45 +0100

    reduce shadow

--- home/programs/hyprland.nix
+++ home/programs/hyprland.nix
@@ -78,7 +78,7 @@
 
         shadow_ignore_window = true;
         shadow_offset = "0 2";
-        shadow_range = 20;
+        shadow_range = 2;
         shadow_render_power = 3;
 #        "col.shadow" = "rgba(00000055)";
       };
