a82d4f786b644db2a8c285e0d91dfbb39e609c12
cratedev <matt@crate.dev>
2024-10-20 20:49:25 +0100

    startup test

--- home/programs/niri.nix
+++ home/programs/niri.nix
@@ -3,7 +3,7 @@
     programs.niri = {
 #        enable = true;
         settings = {
-            spawn-at-startup = "swaybg -i ../wallpaper/2.png"
+            spawn-at-startup = "swaybg -i ../wallpaper/2.png";
 #                { command = [ "${lib.getExe pkgs.networkmanagerapplet}" ]; }
 #                { command = [ "${pkgs.lxqt.lxqt-policykit}/bin/lxqt-policykit-agent" ]; }   # authentication prompts
 #                { command = [ "${lib.getExe pkgs.wl-clip-persist} --clipboard primary" ]; } # to fix wl clipboards disappearing
