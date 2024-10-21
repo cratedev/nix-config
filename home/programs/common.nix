aebfe807d050826ec6c67e7cd0ed7840b9155d3d
cratedev <matt@crate.dev>
2024-10-21 20:33:25 +0100

    libgtop

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -1,6 +1,7 @@
 { lib, pkgs, inputs, ... }:
 {
   home.packages = [
+    pkgs.libgtop
     pkgs.cliphist
     pkgs.wl-clipboard-rs
     pkgs.zip
--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -11,7 +11,7 @@
       garbage = "nix-collect-garbage";
       garbages = "sudo nix-collect-garbage";
       garbaged = "nix-collect-garbage -d";
-      garbagesd = "nix-collect-garbage -d";
+      garbagesd = "sudo nix-collect-garbage -d";
       cheat = "echo 'Ctrl+T,C - Rename tab'; echo 'Ctrl+B,C - New Tab'; echo 'Ctrl+B, K - Up Pane'; echo 'Ctrl+B, J - Down Pane'";
     };
   };
