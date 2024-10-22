798bc78fc20edcdc2ff883ce6584e5ca376c156d
cratedev <matt@crate.dev>
2024-10-22 17:26:59 +0100

    rofi

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,5 +2,19 @@
 {
   nixpkgs.overlays = [
 #    inputs.hyprpanel.overlay
+
+# Rofi?
+
+    (self: super:
+    {
+      rofi = super.rofi.overrideAttrs (oldAttrs: rec {
+        src = super.fetchFromGithub {
+          owner = "Ibonn";
+          repo = "rofi";
+          rev = "142e78071cbd7ddc2228cc707a583e081ec3bdf2";
+          sha256 = "142e78071cbd7ddc2228cc707a583e081ec3bdf2";
+        };
+      });
+    })
   ];
 }
