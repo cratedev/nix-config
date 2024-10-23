c7355d46cdc8549477990f898eb768dd5665110f
cratedev <matt@crate.dev>
2024-10-23 13:46:55 +0100

    rofi

--- home/programs/niri.nix
+++ home/programs/niri.nix
@@ -126,7 +126,7 @@
 
             "Mod+Return".action = spawn "foot" "-e" "zellij" "attach" "--create" "main";
 #	    "Mod+Return".action = spawn "foot -e zellij attach --create main";
-            "Mod+D".action = sh "../../scripts/launcher_t1";
+            "Mod+D".action = spawn "$HOME/.config/rofi/scripts/launcher_t1";
 #            "Mod+D".action = spawn "fuzzel";
 
             "Mod+Q".action = close-window;
