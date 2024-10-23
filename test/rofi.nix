9339e2e9c431d252205135580e0b53d4bbc05ac5
cratedev <matt@crate.dev>
2024-10-23 08:55:52 +0100

    rofi pkg

---
+++ test/rofi.nix
@@ -0,0 +1,21 @@
+let
+    pkgs = import <nixpkgs> { };
+in
+
+with pkgs;
+
+stdenv.mkDerivation {
+    pname = "rofi";
+    version = "1.7.4"
+
+    src ../../git/rofi;
+
+    buildPhase = ''
+        meson setup build
+        ninja -C build
+    '';
+
+    installPhase = ''
+        ninja -C build install
+    '';
+}
