d1b5ba210a01df8c9c96a6ab2aa4e0db5ba8ad5f
cratedev <matt@crate.dev>
2024-11-19 09:02:25 +0000

    nas test

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -11,6 +11,10 @@
       garbaged = "nix-collect-garbage -d";
       garbagesd = "sudo nix-collect-garbage -d";
       cheat = "echo 'Ctrl+T,C - Rename tab'; echo 'Ctrl+B,C - New Tab'; echo 'Ctrl+B, K - Up Pane'; echo 'Ctrl+B, J - Down Pane'";
+      nrsl = "sudo nixos-rebuild switch --flake .#crate-laptop";
+      nrtl = "sudo nixos-rebuild test --flake .#crate-laptop";
+      nrsd = "sudo nixos-rebuild switch --flake .#crate-desktop";
+      nrtd = "sudo nixos-rebuild test --flake .#crate-desktop";
     };
   };
 }
--- modules/nas/default.nix
+++ modules/nas/default.nix
@@ -1,11 +1,5 @@
-{ ... }:
-{
+{...}: {
   imports = [
-    ./sonarr.nix
-    ./radarr.nix
-#    ./jellyfin.nix
-    ./prowlarr.nix
-#    ./homepage.nix
-    ./sabnzbd.nix
+    komodo.nix
   ];
 }
