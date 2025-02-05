bd060c654bdfc57179227409e868a134e1b8eed7
cratedev <matt@crate.dev>
2025-02-05 15:49:41 -0500

    unraid remote mount
    Moved to the host temporarily until all systems are on the same subnet

--- hosts/crate-laptop/default.nix
+++ hosts/crate-laptop/default.nix
@@ -17,6 +17,18 @@
     firewall.enable = false;
     firewall.checkReversePath = "loose";
   };
+
+  # ============================= SSHFS =========================================
+  fileSystems."/home/matt/unraid-ssh" = {
+    device = "root@10.0.0.10:/mnt";
+    fsType = "fuse.sshfs";
+    options = [
+      "nodev"
+      "noatime"
+      "allow_other"
+      "IdentityFile=/home/matt/.ssh/id_ed25519"
+    ];
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
