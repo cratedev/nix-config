43744a84581bb2f734370ae0984950ee1fda61af
cratedev <matt@crate.dev>
2025-02-04 19:05:58 -0500

    fuck fabric

--- home/programs/fabric-cli/default.nix
+++ home/programs/fabric-cli/default.nix
@@ -1,30 +1,39 @@
-with (import <nixpkgs> {});
-  stdenv.mkDerivation rec {
-    pname = "fabric-cli";
-    version = "v0.1";
+{pkgs ? import <nixpkgs> {}}:
+pkgs.stdenv.mkDerivation rec {
+  pname = "fabric-cli";
+  version = "v0.1";
 
-    src = pkgs.lib.cleanSourceWith {
-      src = fetchFromGitHub {
-        owner = "Fabric-Development";
-        repo = "fabric-cli";
-        rev = "f8a1576eb42c4dda9ef38fc96d34573a81cac46a";
-        sha256 = "sha256-0PVdUlugBLBmLroWepQGXr5de83Nnryam1BleAcVFFw=";
-      };
-      filter = path: type: builtins.match ".*vendor/.*" path != null || pkgs.lib.cleanSourceFilter path type;
+  # Add this line to mark build as impure (allowing network access)
+  __impure = true;
+
+  src = pkgs.lib.cleanSourceWith {
+    src = pkgs.fetchFromGitHub {
+      owner = "Fabric-Development";
+      repo = "fabric-cli";
+      rev = "f8a1576eb42c4dda9ef38fc96d34573a81cac46a";
+      sha256 = "sha256-0PVdUlugBLBmLroWepQGXr5de83Nnryam1BleAcVFFw=";
     };
+    filter = path: type: builtins.match ".*vendor/.*" path == null && pkgs.lib.cleanSourceFilter path type;
+  };
 
-    buildInputs = [go];
+  buildInputs = [pkgs.go pkgs.git];
 
-    buildPhase = ''
-      export HOME=$TMPDIR
-      export GOPATH=$TMPDIR/go
-      export GOCACHE=$TMPDIR/go-cache
-      export GOMODCACHE=$TMPDIR/go-mod
-      go build -mod=vendor -o fabric-cli .
-    '';
+  # Add these lines to allow network access during build
+  outputHashMode = "recursive";
+  outputHashAlgo = "sha256";
 
-    installPhase = ''
-      mkdir -p $out/bin
-      cp fabric-cli $out/bin/
-    '';
-  }
+  buildPhase = ''
+    export HOME=$TMPDIR
+    export GOPATH=$TMPDIR/go
+    export GOCACHE=$TMPDIR/go-cache
+    export GOMODCACHE=$TMPDIR/go-mod
+    export GOPROXY=https://proxy.golang.org
+    export CGO_ENABLED=0
+    go build -mod=readonly -o fabric-cli .
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp fabric-cli $out/bin/
+  '';
+}
